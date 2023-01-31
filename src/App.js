60e54c2c13fb72a40049a34b80a4364f6b0169b0
ゆう <yu2000212@icloud.com>
2023-01-31 16:39:40 +0000

    useState

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { useState } from 'react/cjs/react.production.min';
 import ColorfulMessage from './components/ColorfulMessage';
 
 const App = () => {
-    const onClickButton = () => alert();
+    const onClickCountUp = () => {
+        setNum(num + 1);
+    };
+    const [num, setNum] = useState(0);
     return (
         <>
             <h1 style={{ color: 'red' }}>こんにちは</h1>
             <ColorfulMessage color="bule">お元気ですか？</ColorfulMessage>
             <ColorfulMessage color="pink">お元気です！</ColorfulMessage>
-            <button onClick={onClickButton}>ボタン</button>
+            <button onClick={onClickCountUp}>カウントアップ</button>
         </>
     );
 };
