b9bfd5d66902ca3a53808283ab28fb464ccf6688
ゆう <yu2000212@icloud.com>
2023-01-31 15:45:16 +0000

    css style方法

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-
-// ReactはJSの中でhtmlタグを書いてreturnするJSX記法
-const App = () => {
-    return (
-        // Fragment：余計な要素（div）がレンダリングされない
-        // Reactは一つのタグで囲んであげないとエラーが出る。
-        <React.Fragment>
-            <h1>こんにちは</h1>
-            <p>お元気ですか？</p>
-        </React.Fragment>
-    ); 
-};
+import App from './App';
 
 ReactDom.render(<App />, document.getElementById("root"));
-
